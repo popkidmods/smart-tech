3a86615fae67a244aaa375949dcf630ee8620354
carl24tecch <newgitbase@gmail.com>
2024-11-03 11:38:36 +0300

    🕵️CARLTECH🕵

--- scs/restart.js
+++ scs/restart.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("BMW-MD bot Restarting ⏳");
+    repondre("bot Restarting ⏳");
 
   exec("pm2 restart all");
   
