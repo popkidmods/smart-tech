4a668fa5b453b775a80cb8be72393bb4014cd3c5
carl24tecch <newgitbase@gmail.com>
2024-11-03 10:40:56 +0300

    🕵️CARLTECH🕵

--- scs/general.js
+++ scs/general.js
@@ -3,7 +3,7 @@ const { adams } = require("../Ibrahim/adams");
 const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../lib/sudo")
 const conf = require("../config");
 
-adams({ nomCom: "owner", categorie: "General", reaction: "🚘" }, async (dest, zk, commandeOptions) => {
+adams({ nomCom: "owner", categorie: "General", reaction: "🪀" }, async (dest, zk, commandeOptions) => {
     const { ms , mybotpic } = commandeOptions;
     
   const thsudo = await isSudoTableNotEmpty()
@@ -52,17 +52,17 @@ adams({ nomCom: "owner", categorie: "General", reaction: "🚘" }, async (dest, 
   }
 });
 
-adams({ nomCom: "dev", categorie: "General", reaction: "🚘" }, async (dest, zk, commandeOptions) => {
+adams({ nomCom: "dev", categorie: "General", reaction: "🪀" }, async (dest, zk, commandeOptions) => {
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
-      { nom: "Ibrahim", numero: "254710772666" },
-      { nom: "᚛Baraka᚜", numero: "255762190568" },
-      { nom: "Beltah", numero: "254114141192" },
+      { nom: "Carl", numero: "254770954948" },
+      { nom: "᚛CARLTECH᚜", numero: "254770954948" },
+      { nom: "all", numero: "254770954948" },
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "WELCOME TO BMW HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:\n\n";
+    let message = "WELCOME TO OUR HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:\n\n";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }
@@ -96,7 +96,7 @@ else {
 adams({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
-  repondre("THANK YOU FOR CHOOSING BMW, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://chat.whatsapp.com/F5BXJci8EDS9AJ6sfKMXIS] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://www.youtube.com/@ibrahimaitech] \n\n\n𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔") 
-  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING BMW-MD,MAKE SURE YOU FOLLOW THESE LINKS. `},{quoted :ms})
+  repondre("HELLO ESTEEMED USER, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029Vak0genJ93wQXq3q6X3h] \n\n ☉ TELEGRAM LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://t.me/carltechai] \n\n ☉TG IS HERE ☉\n\n❒⁠⁠⁠⁠[https://t.me/carlltecch] \n\n\ncarltech") 
+  await zk.sendMessage(auteurMessage,{text : `MAKE SURE YOU FOLLOW THESE LINKS IF YOU DON'T MIND. `},{quoted :ms})
 
 })
