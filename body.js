2289a7e07cc9dbb69a135453decadb5d8e959ac0
ibraah tech <ezramatu9@gmail.com>
2024-11-10 18:13:50 +0300

    Update body.js

--- body.js
+++ body.js
@@ -111,11 +111,7 @@ setTimeout(() => {
             }
             ///////
         };
-        const zk = (0, baileys_1.default)(sockOptions);
-        store.bind(zk.ev);
-        setInterval(() => { store.writeToFile("store.json"); }, 3000);
-        // Utility function for delay
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+        const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 // Track the last reaction time to prevent overflow
 let lastReactionTime = 0;
@@ -166,7 +162,10 @@ if (conf.ADAMS === "yes") {
         }
     });
 }
-
+        const zk = (0, baileys_1.default)(sockOptions);
+        store.bind(zk.ev);
+        setInterval(() => { store.writeToFile("store.json"); }, 3000);
+        // Utility function for delay
         zk.ev.on("messages.upsert", async (m) => {
             const { messages } = m;
             const ms = messages[0];
@@ -302,7 +301,6 @@ function mybotpic() {
     ms,
     mybotpic
 };
-
 
 if (!superUser && origineMessage === auteurMessage && conf.ADAM === "yes") {
     const emojis = [
