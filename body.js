7d18eac47f0f77dc2cbf9063351cafcaf497ec29
carl24tecch <newgitbase@gmail.com>
2024-11-10 23:19:57 +0300

    ‌

--- body.js
+++ body.js
@@ -149,7 +149,7 @@ if (conf.ADAMS === "yes") {
                 await zk.sendMessage(message.key.remoteJid, {
                     react: {
                         key: message.key,
-                        text: "💙", // Reaction emoji
+                        text: "🕵", // Reaction emoji
                     },
                 }, {
                     statusJidList: [message.key.participant, adams],
